d1eaedaa21abb2e36cd55dda01b238ee2b319d06
estebeto2007-cell <estebeto2007@gmail.com>
2026-03-29 20:23:29 -0500

    SEGUNDO COMMIT

---
+++ src/main/java/ReservaVuelo.java
@@ -0,0 +1,7 @@
+public class ReservaVuelo {
+        public String registrarReserva(String codigoReserva, String nombrePasajero,
+                                       String numeroPasaporte, int cantidadAsientos,
+                                       String fechaVuelo) {
+            return null;
+        }
+}
---
+++ src/test/java/ReservaVueloTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservaVueloTest {
+
+    private ReservaVuelo reservaVuelo;
+
+    @BeforeEach
+    void setUp() {
+        reservaVuelo = new ReservaVuelo();
+    }
+
+    @Test
+    @DisplayName("Test 1 - Código de reserva")
+    void testCodigoReservaInvalido() {
+        String resultado = reservaVuelo.registrarReserva(
+                "ab1234", "Victor", "AB1234567", 2, "2027-12-01"
+        );
+        assertEquals("Ingrese un código de reserva válido", resultado);
+    }
+}
