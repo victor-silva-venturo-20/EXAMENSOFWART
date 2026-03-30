750f8698891326e733e68ab94a2322c92c788470
estebeto2007-cell <estebeto2007@gmail.com>
2026-03-29 20:56:36 -0500

    .

--- src/test/java/ReservaVueloTest.java
+++ src/test/java/ReservaVueloTest.java
@@ -17,8 +17,61 @@ class ReservaVueloTest {
     @DisplayName("Test 1 - Código de reserva")
     void testCodigoReservaInvalido() {
         String resultado = reservaVuelo.registrarReserva(
-                "ab1234", "Victor", "AB1234567", 2, "2027-12-01"
+                "AC1234", "Victor", "AB1234567", 2, "2026-03-20"
         );
         assertEquals("Ingrese un código de reserva válido", resultado);
     }
+
+    @Test
+    @DisplayName("Test 2 - Nombre con menos de 5 caracteres")
+    void testNombrePasajeroInvalido() {
+        String resultado = reservaVuelo.registrarReserva(
+                "AB1234", "Ana", "AB1234567", 2, "2026-03-01"
+        );
+        assertEquals("El nombre del pasajero debe tener al menos cinco caracteres alfabéticos", resultado);
+    }
+
+    @Test
+    @DisplayName("Test 3 - Número de pasaporte inválido")
+    void testNumeroPasaporteInvalido() {
+        String resultado = reservaVuelo.registrarReserva(
+                "ABC123", "Carlos", "AB123", 2, "2026-03-03"
+        );
+        assertEquals("Ingrese un número de pasaporte válido", resultado);
+    }
+
+    @Test
+    @DisplayName("Test 4 - Cantidad de asientos menor o igual a 0")
+    void testCantidadAsientosInvalida() {
+        String resultado = reservaVuelo.registrarReserva(
+                "AB1234", "Victor", "AB1234567", 0, "2026-03-01"
+        );
+        assertEquals("La cantidad de asientos debe ser mayor a cero", resultado);
+    }
+
+    @Test
+    @DisplayName("Test 5 - Fecha del vuelo anterior")
+    void testFechaVueloInvalida() {
+        String resultado = reservaVuelo.registrarReserva(
+                "AB1234", "Victor", "AB1234567", 2, "2020-03-20"
+        );
+        assertEquals("La fecha del vuelo debe ser posterior a la fecha actual", resultado);
+    }
+
+    @Test
+    @DisplayName("Test 6 - Reserva exitosa y datos vacíos")
+    void testReservaExitosaYCamposVacios() {
+        String resultado1 = reservaVuelo.registrarReserva(
+                "AB1234", "Victor", "AB1234567", 2, "2026-03-15"
+        );
+        assertEquals("La reserva ha sido registrada correctamente", resultado1);
+
+        String resultado2 = reservaVuelo.registrarReserva(
+                "", "", "", 0, ""
+        );
+        assertEquals("Debe ingresar todos los datos requeridos", resultado2);
+    }
+
+
+
 }
